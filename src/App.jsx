fac554125b78de0e1dc77b0762643bb6e47418f8
Mehraj Hosen <meherajhosen786@gmail.com>
2025-06-17 03:18:25 +0600

    img add

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
+import Flex from './layouts/Flex'
+import image from './assets/registerImg.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+<>
+<Flex>
+  <div className="left w-[55%]">
+    <h1>Get started with easily register</h1>
+    <p>Free register and you can enjoy it</p>
+  </div>
+  <div className="right w-[45%] object-cover h-screen">
+    <img className='w-full object-cover h-full' src={image} alt="" />
+  </div>
+</Flex>
+
+</>
   )
 }
 
