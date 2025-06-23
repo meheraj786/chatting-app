aecfa5a4b992137d7c4174489273754b591cdec8
Mehraj Hosen <meherajhosen786@gmail.com>
2025-06-24 00:18:33 +0600

    log in page done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,13 +4,12 @@ import image from "../assets/registerImg.png";
 import { LuEyeClosed } from "react-icons/lu";
 import { LuEye } from "react-icons/lu";
 import { Link } from 'react-router';
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
   const [email, setEmail] = useState("");
-  const [fullName, setFullName] = useState("");
   const [password, setPassword] = useState("");
   const [emailErr, setEmailErr] = useState("");
-  const [fullNameErr, setFullNameErr] = useState("");
   const [passwordErr, setPasswordErr] = useState("");
   const [passShow, setPassShow]= useState(false)
 
@@ -20,21 +19,14 @@ const Login = () => {
     setEmail(e.target.value);
     setEmailErr("");
   };
-  const fullNameHandler = (e) => {
-    setFullName(e.target.value);
-    setFullNameErr("");
-  };
   const passwordHandler = (e) => {
     setPassword(e.target.value);
     setPasswordErr("");
   };
   const submitHandler = () => {
-    console.log(email + fullName + password);
+    console.log(email + password);
     if (!email) {
       setEmailErr("Enter Your Email");
-    }
-    if (!fullName) {
-      setFullNameErr("Enter Your Full Name");
     }
     if (!password) {
       setPasswordErr("Enter a Password");
@@ -68,20 +60,22 @@ const Login = () => {
   return (
         <>
       <Flex>
-        <Flex className="left flex-col mx-auto xl:mx-0 items-end justify-center  font-primary p-10 xl:p-0 xl:w-[55%] xl:pr-[169px]">
+        <Flex className="left flex-col mx-auto xl:mx-0 items-end justify-center  font-primary p-10 xl:p-0 xl:w-[55%] xl:pr-[250px]">
           <div>
             <h1 className="font-bold text-4xl text-secondary">
-              Get started with easily register
+              Login to your account!
             </h1>
-            <p className="mt-[13px] text-xl mb-10 text-primary/50">
-              Free register and you can enjoy it
-            </p>
-            <div className="relative">
+<Flex className="google cursor-pointer gap-2 mt-[30px] items-baseline py-[23px] text-[14px] font-secondary text-secondary font-semibold justify-center w-[221px] rounded-[16px] border border-gray-300">
+  <FcGoogle className="text-xl" />
+  <p>Loging with Google</p>
+
+</Flex>
+            <div className="relative mt-[32px]">
               <input
                 type="text"
                 onChange={emailHandler}
                 id="floating_outlined"
-                className="block w-full px-[26px] py-[26px] xl:w-[368px] text-xl text-secondary font-semibold bg-transparent rounded-lg border-1 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-secondary/30 peer"
+                className="block w-full px-[26px] py-[26px] xl:w-[368px] text-xl text-secondary font-semibold bg-transparent rounded-lg border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-secondary/30 peer"
                 placeholder=" "
               />
               <label
@@ -92,33 +86,18 @@ const Login = () => {
               </label>
             </div>
             <p className="text-red-500 mx-2">{emailErr}</p>
-            <div className="relative mt-[32px]">
-              <input
-                type="text"
-                onChange={fullNameHandler}
-                id="floating_outlined2"
-                className="block w-full px-[26px] py-[26px] xl:w-[368px] text-xl text-secondary font-semibold bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-secondary/30 peer"
-                placeholder=""
-              />
-              <label
-                for="floating_outlined2"
-                className="absolute text-sm text-secondary/70 duration-300 transform  -translate-y-4 top-2 z-10 origin-[0] bg-white  px-4 peer-focus:px-4 peer-focus:text-secondary/70  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2  peer-focus:-translate-y-4 rtl:peer-focus:translate-x-2/5 rtl:peer-focus:left-auto start-4"
-              >
-                Full Name
-              </label>
-            </div>
-            <p className="text-red-500 mx-2">{fullNameErr}</p>
+
             <div className="relative mt-[32px]">
               <input
                 type={passShow ? "text": "password"}
                 onChange={passwordHandler}
                 id="floating_outlined3"
-                className="block w-full px-[26px] xl:w-[368px] py-[26px]  text-xl text-secondary font-semibold bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-secondary/30 peer"
+                className="block w-full px-[26px] xl:w-[368px] py-[26px]  text-xl text-secondary font-semibold bg-transparent rounded-lg border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-secondary/30 peer"
                 placeholder=" "
               />
               {
-                passShow ? <LuEye onClick={()=> setPassShow(!passShow)} className="text-2xl absolute top-1/2 -translate-y-1/2 cursor-pointer right-5 xl:right-[35%] text-primary" />
- : <LuEyeClosed onClick={()=> setPassShow(!passShow)} className="text-2xl cursor-pointer absolute top-1/2 -translate-y-1/2 right-5 xl:right-[35%] text-primary" />
+                passShow ? <LuEye onClick={()=> setPassShow(!passShow)} className="text-2xl absolute top-1/2 -translate-y-1/2 cursor-pointer right-5 xl:right-[5%] text-primary" />
+ : <LuEyeClosed onClick={()=> setPassShow(!passShow)} className="text-2xl cursor-pointer absolute top-1/2 -translate-y-1/2 right-5 xl:right-[5%] text-primary" />
               }
 
               <label
@@ -128,16 +107,16 @@ const Login = () => {
                 Password
               </label>
             </div>
-            <p className="text-red-500 mx-2">{passwordErr}</p>
+            <p className="text-red-500 xl:w-[368px] mx-2">{passwordErr}</p>
             <button
               onClick={submitHandler}
-              className="xl:w-[368px] w-full cursor-pointer py-5 bg-primary text-white font-semibold mt-[51px] mb-[35px] rounded-[86px] text-xl"
+              className="xl:w-[368px] w-full cursor-pointer py-5 bg-primary text-white font-semibold mt-[51px] mb-[35px] rounded-[9px] text-xl"
             >
-              Sign Up
+              Login to Continue
             </button>
-            <p className="xl:w-[368px] font-secondary text-center text-[14px] text-primary">
-              Already have an account ?{" "}
-              <Link to="/registration" className="text-[#EA6C00] cursor-pointer">Sign Up</Link>
+            <p className="xl:w-[368px] font-secondary text-left text-[14px] text-primary">
+              Don’t have an account ? {" "}
+              <Link to="/registration" className="text-[#EA6C00] cursor-pointer">Sign up</Link>
             </p>
           </div>
         </Flex>
