a31b3e0678884258e878697384cfb527989c2878
meheraj786 <meherajhosen786@gmail.com>
2025-07-22 03:26:50 +0600

    group add

--- src/components/blockedUser/BlockedUser.jsx
+++ src/components/blockedUser/BlockedUser.jsx
@@ -3,6 +3,7 @@ import Flex from "../../layouts/Flex";
 import SearchInput from "../../layouts/SearchInput";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import Button from "../../layouts/Button";
+import userImg from "../../assets/user.png";
 // import userImg1 from "../../assets/user1.png";
 // import userImg2 from "../../assets/user2.png";
 // import userImg3 from "../../assets/user3.png";
@@ -10,13 +11,13 @@ import Button from "../../layouts/Button";
 // import userImg5 from "../../assets/user5.png";
 import { getDatabase, onValue, ref, remove } from "firebase/database";
 import { useSelector } from "react-redux";
+import UserSkeleton from "../skeleton/UserSkeleton";
 
 const BlockedUser = () => {
   const db = getDatabase();
   const [blockList, setBlockList] = useState([]);
   const data = useSelector((state) => state.userInfo.value);
   const [blockListLoading, setBlockListLoading] = useState(true);
-
 
   useEffect(() => {
     const requestRef = ref(db, "blocklist/");
@@ -37,9 +38,9 @@ const BlockedUser = () => {
     });
   }, []);
 
-  const unBlockHandler=(id)=>{
+  const unBlockHandler = (id) => {
     remove(ref(db, "blocklist/" + id));
-  }
+  };
 
   return (
     <div className="xl:w-[30%] w-full shadow-shadow h-[50%] rounded-[20px] px-[20px] font-poppins py-[20px]">
@@ -51,34 +52,45 @@ const BlockedUser = () => {
       {/* <SearchInput /> */}
 
       <div className="overflow-y-auto h-[90%]">
-        {blockList.map((user, idx) => (
-          <Flex
-            key={idx}
-            className="py-[10px] border-b-2 border-gray-300 items-center justify-between"
-          >
-            <Flex className="gap-x-[14px] w-[60%] items-center justify-start">
-              <div>
-                <img
-                  src=""
-                  className="avatar w-[52px] h-[52px] rounded-full"
-                  alt=""
-                />
-              </div>
+        {blockListLoading ? (
+          <UserSkeleton />
+        ) : (
+          blockList.map((user, idx) => (
+            <Flex
+              key={idx}
+              className="py-[10px] border-b-2 border-gray-300 items-center justify-between"
+            >
+              <Flex className="gap-x-[14px] w-[60%] items-center justify-start">
+                <div>
+                  <img
+                    src={userImg}
+                    className="avatar border w-[52px] h-[52px] rounded-full"
+                    alt=""
+                  />
+                </div>
 
-              <div className="w-[60%]">
-                <h3 className="text-[14px] font-semibold text-black truncate w-full">
-                  {user.blockerId==data.uid ? user.blockedName: user.blockerName}
-                </h3>
-                <p className="text-[10px] text-black/50 truncate w-full">
-                  12:27
-                </p>
-              </div>
+                <div className="w-[60%]">
+                  <h3 className="text-[14px] font-semibold text-black truncate w-full">
+                    {user.blockerId == data.uid
+                      ? user.blockedName
+                      : user.blockerName}
+                  </h3>
+                  <p className="text-[10px] text-black/50 truncate w-full">
+                    12:27
+                  </p>
+                </div>
+              </Flex>
+              {user.blockerId == data.uid && (
+                <Button
+                  onClick={() => unBlockHandler(user.id)}
+                  className="text-[12px]"
+                >
+                  Unblock
+                </Button>
+              )}
             </Flex>
-            {user.blockerId == data.uid && (
-              <Button onClick={()=>unBlockHandler(user.id)} className="text-[12px]">Unblock</Button>
-            )}
-          </Flex>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
--- src/components/groupList/GroupList.jsx
+++ src/components/groupList/GroupList.jsx
@@ -1,64 +1,129 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Flex from '../../layouts/Flex'
 import SearchInput from '../../layouts/SearchInput'
 import { BsThreeDotsVertical } from 'react-icons/bs'
 import Button from '../../layouts/Button'
 import grpImg1 from '../../assets/grpImg1.png'
+import grpImg from '../../assets/groupImg.png'
 import grpImg2 from '../../assets/grpImg2.png'
 import grpImg3 from '../../assets/grpImg3.png'
 import grpImg4 from '../../assets/grpImg4.jpg'
 import grpImg5 from '../../assets/grpImg5.jpg'
+import { useSelector } from 'react-redux'
+import { getDatabase, onValue, push, ref, set } from 'firebase/database'
+import { toast } from 'react-toastify'
+import UserSkeleton from '../skeleton/UserSkeleton'
 
 const GroupList = () => {
-  const groups = [
-    {
-      img: grpImg1,
-      grpName: "Friends Reunion",
-      lastMsg: "Hi Guys, Wassup!"
-    },
-    {
-      img: grpImg2,
-      grpName: "Friends Forever",
-      lastMsg: "Good to see you"
-    },
-    {
-      img: grpImg3,
-      grpName: "Crazy Cousins",
-      lastMsg: "What plans today?"
-    },
-    {
-      img: grpImg4,
-      grpName: "Office Group",
-      lastMsg: "Join the meeting"
-    },
-    {
-      img: grpImg5,
-      grpName: "Gaming Group",
-      lastMsg: "Let's play"
-    },
-  ];
+  const db= getDatabase()
+  const [createGroup, setCreateGroup]=useState(false)
+    const data = useSelector((state) => state.userInfo.value);
+    const [groups, setGroups]=useState([])
+    const [groupListLoading, setGroupListLoading]=useState(true)
+  const [groupName, setGroupName]=useState("")
+  // const groups = [
+  //   {
+  //     img: grpImg1,
+  //     grpName: "Friends Reunion",
+  //     lastMsg: "Hi Guys, Wassup!"
+  //   },
+  //   {
+  //     img: grpImg2,
+  //     grpName: "Friends Forever",
+  //     lastMsg: "Good to see you"
+  //   },
+  //   {
+  //     img: grpImg3,
+  //     grpName: "Crazy Cousins",
+  //     lastMsg: "What plans today?"
+  //   },
+  //   {
+  //     img: grpImg4,
+  //     grpName: "Office Group",
+  //     lastMsg: "Join the meeting"
+  //   },
+  //   {
+  //     img: grpImg5,
+  //     grpName: "Gaming Group",
+  //     lastMsg: "Let's play"
+  //   },
+  // ];
+  const createGroupHandler=()=>{
+    set(push(ref(db, "grouplist/")), {
+          groupName: groupName,
+          creatorId: data.uid
+        });
+        setGroupName("")
+        setCreateGroup(false)
+        toast.success("Group Created");
+  }
+
+    useEffect(() => {
+      const groupList = ref(db, "grouplist/");
+      onValue(groupList, (snapshot) => {
+        let arr = [];
+        snapshot.forEach((group) => {
+          const groupItem= group.val()
+          const groupId=group.key
+          arr.push({...groupItem, id:groupId});
+        });
+        setGroups(arr);
+        setGroupListLoading(false);
+      });
+    }, []);
+    
 
   return (
     <div className='xl:w-[36%] w-full shadow-shadow h-[50%] rounded-[20px] px-[20px] font-poppins py-[20px]'>
+      {
+        createGroup && <div className='absolute  top-1/2 left-1/2 -translate-1/2 flex justify-center items-center w-full z-[9999] h-full bg-gray-200/10 backdrop-blur-[3px]'>
+          <div className=' w-1/2 flex flex-col justify-center items-center h-1/2 relative bg-white shadow-2xl'> 
+            <span onClick={()=>setCreateGroup(false)} className="absolute font-semibold text-[22px] cursor-pointer right-10 top-10">X</span>
+          <div className="relative ">
+          <input
+            type="text"
+            value={groupName}
+            onChange={(e)=>setGroupName(e.target.value)}
+            id="floating_outlined"
+            className="block w-full px-[26px] py-[26px] xl:w-[368px] text-xl text-secondary font-semibold bg-transparent rounded-lg border-0 border-b-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-secondary/30 peer"
+            placeholder=" "
+          />
+          <label
+            for="floating_outlined"
+            className="absolute text-sm text-secondary/70 duration-300 transform  top-2 z-10 origin-[0] bg-white  -translate-y-4 px-4 peer-focus:px-4 peer-focus:text-secondary/70  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2  peer-focus:-translate-y-4 rtl:peer-focus:translate-x-2/5 rtl:peer-focus:left-auto start-4"
+          >
+            Group Name
+          </label>
+        </div>
+<Button onClick={createGroupHandler} className="mt-10">Create Group</Button>
+          
+          </div>
+        </div>
+      }
       <Flex className="justify-between items-center mb-2">
         <h3 className='text-[20px] font-semibold text-black'>Group List</h3>
-        <BsThreeDotsVertical />
+        {/* <BsThreeDotsVertical /> */}
+        <Button onClick={()=>setCreateGroup(true)} >Create Group</Button>
       </Flex>
 
       {/* <SearchInput /> */}
       <div className='overflow-y-auto h-[90%]'>
         {
-          groups.map((group, i) => (
+          groupListLoading ? <>
+          <UserSkeleton/>
+          <UserSkeleton/>
+          <UserSkeleton/>
+          </> : groups.map((group, i) => (
             <Flex key={i} className="py-[13px] border-b-2 border-gray-300 items-center justify-between">
               <Flex className="gap-x-[14px] w-[70%] items-center justify-start">
                 <div
 
                 >
-                                    <img src={group.img}  className='avatar w-[70px] h-[70px]  rounded-full' alt="" />
+                                    <img src={grpImg}  className='avatar border w-[70px] h-[70px]  rounded-full' alt="" />
                 </div>
 
                 <div className="w-[60%]">
-                  <h3 className='text-[20px] font-semibold text-black truncate w-full'>{group.grpName}</h3>
+                  <h3 className='text-[20px] font-semibold text-black truncate w-full'>{group.groupName}</h3>
                   <p className='font-medium text-[14px] text-[#4D4D4D]/75 truncate w-full'>{group.lastMsg}</p>
                 </div>
               </Flex>
