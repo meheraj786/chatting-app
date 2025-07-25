8b54136274347749f612680c593d8ef04602bb5b
meheraj786 <meherajhosen786@gmail.com>
2025-07-25 23:34:12 +0600

    time function add

--- src/components/time/time.js
+++ src/components/time/time.js
@@ -1,15 +1,17 @@
+const time = () => {
+  const now = new Date();
 
-const time=()=>{
-const now = new Date();
-const formatted = now.toLocaleString("en-GB", {
-  hour: "2-digit",
-  minute: "2-digit",
-  year: "numeric",
-  month: "long",
-  day: "2-digit",
-  hour12: true,
-});
-return formatted
-}
+  const hour = now.toLocaleString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
 
-export default time
+  const day = now.getDate().toString().padStart(2, "0"); 
+  const month = now.toLocaleString("en-GB", { month: "long" });
+  const year = now.getFullYear();
+
+  return `${hour}, ${day} ${month}, ${year}`;
+};
+
+export default time;
